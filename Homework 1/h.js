edb8f90876cbed317364eda26fcd73c14e32fd63
yasuomaidana <yasuomaidana@gmail.com>
2021-09-16 13:59:03 -0500

    CSS course excersices done

---
+++ Homework 1/h.js
@@ -0,0 +1,10 @@
+$(document).ready(()=>{
+    loadLinks();
+});
+loadLinks=()=>{
+    let links="";
+    for(let i=1;i<=3;i++){
+        links+=`<a href="https://www.google.com/search?q=link+${i}" target="_blank">Link ${i}</a>&nbsp;`;
+    }
+    $("header nav").html(links);
+}
