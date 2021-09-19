394f531a2b8b4863a09b3606512116d4c028a317
yasuomaidana <yasuomaidana@gmail.com>
2021-09-19 13:55:14 -0500

    Gradient and opacity added

--- Homework 1/h.js
+++ Homework 1/h.js
@@ -3,7 +3,7 @@ $(document).ready(()=>{
 });
 loadLinks=()=>{
     let links="";
-    for(let i=1;i<=3;i++){
+    for(let i=1;i<=4;i++){
         links+=`<a href="https://www.google.com/search?q=link+${i}" target="_blank">Link ${i}</a>&nbsp;`;
     }
     $("header nav").html(links);
